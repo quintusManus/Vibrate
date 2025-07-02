1029c0289b39d455fa511544ef8ff78f42b53814
Benjamin Woods <107276939+quintusManus@users.noreply.github.com>
2025-07-02 16:51:29 -0400

    Add Spotify redirect URI support

--- api/login-spotify.js
+++ api/login-spotify.js
@@ -1,10 +1,13 @@
 const querystring = require('querystring');
 
 module.exports = (req, res) => {
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI ||
+    `http://${req.headers.host}/api/callback-spotify`;
+
   const params = querystring.stringify({
     client_id: process.env.SPOTIFY_CLIENT_ID,
     response_type: 'code',
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+    redirect_uri: redirectUri,
     scope: 'playlist-modify-private playlist-modify-public',
     state: Math.random().toString(36).substring(2, 15)
   });
